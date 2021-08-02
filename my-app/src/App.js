2f90922fa8fb988e7fe3637cac818a8bafc1cf5b
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-08-02 14:55:47 -0300

    Criação da tela de login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import login from './icone_login.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+
+        <div class="img" >
+          <img src={login} className="icone-logo" />
+        </div>
+
+        <section class="form-section">
+            <h1>Entrar</h1>
+
+            <div class="form-wrapper">
+              <form>
+                <div class="input-block">
+                  <label for="login-email">Email</label>
+                  <input type="email" id="login-email" />
+                </div>
+                <div class="input-block">
+                  <label for="login-password">Password</label>
+                  <input type="password" id="login-password" />
+                </div>
+                <button type="submit" class="btn-login">Login</button>
+              </form>
+            </div>
+          </section>        
+      </main>
+
     </div>
+
   );
 }
 
