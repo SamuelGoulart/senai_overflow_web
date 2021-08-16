90ba883a64f5357e8cfd6c5a20c0066b41ed6450
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-08-16 07:58:50 -0300

    Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function Page1(){
+  return <h1> Esta é a página 1 </h1>;
+}
+
+function Page2(){
+  return <h1> Esta é a página 1 </h1>;
+}
+
+function App() {
+   let currentPage = 1;
+
+  return (
+    <div className="App">
+       {currentPage === 1 ? <Page1/> : <Page2/>}yar
+    </div>
+  );
+}
+
+export default App;
